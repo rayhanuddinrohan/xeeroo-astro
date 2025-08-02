17feed4ae5a2d707fe04bece7f5e54ae33efeb52
Rayhan Uddin Rohan <100898613+rayhanuddinrohan@users.noreply.github.com>
2025-08-02 18:06:34 +0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from "astro/config";
+
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import github from "@astrojs/github";
 
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://xeeroo.com/"
+  site: 'https://rayhanuddinrohan.github.io/xeeroo-astro/',
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
+  base: '/xeeroo-astro/', // এটা project folder নাম হলে ভালো হয়
 });
